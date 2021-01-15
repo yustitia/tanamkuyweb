0e5f56ea9e83b85acceecc60918209c56811d1b8
Bobby Nugraha Ariffin <bobzzee@gmail.com>
2021-01-15 14:42:57 +0700

    update api kirim

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -21,15 +21,29 @@ class Api extends CI_Controller
 			->row_array();
 
 		$result = [
-			'ph'  => $data['ph'],
-			'ldr' => $data['ldr'],
-			'ppm' => $data['ppm'],
-			'ec'  => $data['ec'],
+			'ph'   => $data['ph'],
+			'ldr'  => $data['ldr'],
+			'ppm'  => $data['ppm'],
+			'ec'   => $data['ec'],
 			'suhu' => $data['suhu'],
 		];
 
 		header('Content-Type: application/json');
     	echo json_encode( $result );
+	}
+
+	public function kirim()
+	{
+		$data = array(
+			'ph'    => $this->input->get('sensor1'),
+			'ldr'   => $this->input->get('sensor2'),
+			'ppm'   => $this->input->get('sensor3'),
+			'ec'    => $this->input->get('sensor4'),
+			'suhu'  => $this->input->get('sensor5'),
+			'waktu' => date("Y-m-d H:i:s"),
+		);
+
+		$this->db->insert('tb_monitoring', $data);
 	}
 
 }
